430810beaf5a8bf3fec8812c232b6cadd3850f33
Matteo Gassend <mgassend@gmail.com>
2025-12-03 19:02:13 +0100

    cleanup

--- apps/web/src/routes/_auth/dashboard.tsx
+++ apps/web/src/routes/_auth/dashboard.tsx
@@ -1,15 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { AddApplicationDialog } from "@/components/add-application-dialog";
 import { Board } from "@/components/board";
-import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
 
 export const Route = createFileRoute("/_auth/dashboard")({
 	component: RouteComponent,
-	beforeLoad: async () => {
-		const { data: customerState } = await authClient.customer.state();
-		return { customerState };
-	},
 });
 
 function RouteComponent() {
